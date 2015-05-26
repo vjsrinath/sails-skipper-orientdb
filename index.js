b3ee66a00bd3e7a4d09efda75ed1d2d591bb464b
vjsrinath <vjsrinath@live.com>
2015-05-26 02:43:56 -0700

    Code cleanup is done

--- index.js
+++ index.js
@@ -75,44 +75,6 @@ module.exports = function OrientDBStore(globalOpts) {
         read: function (fd, cb) {
             console.log("Not implemented yet!!!");
             return;
-            GridStore.exist(db, fd, globalOpts.bucket, function (err, exists) {
-                if (err) {
-                    db.close();
-                    return cb(err);
-                }
-                if (!exists) {
-                    err = new Error('ENOENT');
-                    err.name = 'Error (ENOENT)';
-                    err.code = 'ENOENT';
-                    err.status = 404;
-                    err.message = util.format('No file exists in this mongo gridfs bucket with that file descriptor (%s)', fd);
-                    db.close();
-                    return cb(err);
-                }
-
-                var gridStore = new GridStore(db, fd, 'r', {root: globalOpts.bucket});
-                gridStore.open(function (err, gridStore) {
-                    if (err) {
-                        db.close();
-                        return cb(err);
-                    }
-                    var stream = gridStore.stream();
-                    stream.pipe(concat(function (data) {
-                        db.close();
-                        return cb(null, data);
-                    }));
-
-                    stream.on('error', function (err) {
-                        db.close();
-                        return cb(err);
-                    });
-
-                    stream.on('close', function () {
-                        db.close();
-                    });
-                });
-            });
-
         },
 
         readLastVersion: function (fd, cb) {
@@ -125,67 +87,7 @@ module.exports = function OrientDBStore(globalOpts) {
         readVersion: function (fd, version, cb) {
             console.log("Not implemented yet!!!");
             return;
-            MongoClient.connect(globalOpts.uri, {native_parser: true}, function (err, db) {
-                if (err) {
-                    return cb(err);
-                }
-                var gfs = Grid(db, mongo);
-                gfs.collection(globalOpts.bucket).ensureIndex({filename: 1, uploadDate: -1}, function (err, indexName) {
-                    if (err) {
-                        db.close();
-                        return cb(err);
-                    }
 
-                    var cursor = gfs.collection(globalOpts.bucket).find({filename: fd});
-                    if (version < 0) {
-                        var skip = Math.abs(version) - 1;
-                        cursor.limit(-1).skip(skip).sort({uploadDate: -1}); //'desc
-                    } else {
-                        cursor.limit(-1).skip(version).sort({uploadDate: 1}); //'asc'
-                    }
-
-                    cursor.next(function (err, file) {
-
-                        if (err) {
-                            console.log(err);
-                            db.close();
-                            return cb(err);
-                        }
-                        if (!file) {
-                            err = new Error('ENOENT');
-                            err.name = 'Error (ENOENT)';
-                            err.code = 'ENOENT';
-                            err.status = 404;
-                            err.message = util.format('No file exists in this mongo gridfs bucket with that file descriptor (%s)', fd);
-                            db.close();
-                            return cb(err);
-                        }
-
-                        var gridStore = new GridStore(db, file._id, 'r', {root: globalOpts.bucket});
-                        gridStore.open(function (err, gridStore) {
-                            if (err) {
-                                db.close();
-                                return cb(err);
-                            }
-
-                            var stream = gridStore.stream();
-                            stream.pipe(concat(function (data) {
-                                db.close();
-                                return cb(null, data);
-                            }));
-
-                            stream.on('error', function (err) {
-                                db.close();
-                                return cb(err);
-                            });
-
-                            stream.on('close', function () {
-                                db.close();
-                            });
-                        });
-                    });
-                });
-            });
         },
 
         rm: function (fd, cb) {
@@ -194,13 +96,13 @@ module.exports = function OrientDBStore(globalOpts) {
 
         /**
          * A simple receiver for Skipper that writes Upstreams to
-         * gridfs
+         * orientdb
          *
          *
          * @param  {Object} options
          * @return {Stream.Writable}
          */
-        receive: function GridFSReceiver(options) {
+        receive: function OrientDBReceiver(options) {
             options = options || {};
             options = _.defaults(options, globalOpts);
 
